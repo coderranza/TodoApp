359076007c34d6e98553050dfca19337ee981eac
coderranza <bryancarranza.works@gmail.com>
2023-03-16 21:48:31 -0600

    Item and View Model created.

---
+++ app/src/main/java/com/bcarranza/todoapp/data/Item.kt
@@ -0,0 +1,3 @@
+package com.bcarranza.todoapp.data
+
+data class Item(val id: Int, var title: String, var description: String, var isCompleted: Boolean)
--- app/src/main/java/com/bcarranza/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/bcarranza/todoapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bcarranza.todoapp
+package com.bcarranza.todoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bcarranza.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
